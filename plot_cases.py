bfcdc7b05848ef20a511d4a651820b10126b8931
dkhall <dkhall@users.noreply.github.com>
2020-04-21 07:58:58 -0400

    2020-04-20 updates

--- plot_cases.py
+++ plot_cases.py
@@ -11,30 +11,30 @@ for ii in ma_data.index:
 
 
 fig, ax = plt.subplots()
-plt.semilogy(dates, ma_data['Total Tested'], 'b-')
-plt.semilogy(dates[-1], ma_data['Total Tested'][-1], 'b.')
-ax.annotate('Total Tested: {:,}'.format(int(ma_data['Total Tested'][-1])), (dates[-1]+dt.timedelta(hours=6),
-    0.85*ma_data['Total Tested'][-1]), color='blue', fontsize=9)
+#plt.semilogy(dates, ma_data['Total Tested'], 'b-')
+#plt.semilogy(dates[-1], ma_data['Total Tested'][-1], 'b.')
+#ax.annotate('Total Tested: {:,}'.format(int(ma_data['Total Tested'][-1])), (dates[-1]+dt.timedelta(hours=6),
+#    0.85*ma_data['Total Tested'][-1]), color='blue', fontsize=9)
 
-plt.semilogy(dates, ma_data['Confirmed Cases'], 'k.-')
-plt.semilogy(dates[-1], ma_data['Confirmed Cases'][-1], 'k.')
+plt.semilogy(dates, ma_data['Confirmed Cases'], 'k.')
+#plt.semilogy(dates[-1], ma_data['Confirmed Cases'][-1], 'k.')
 ax.annotate('Confirmed Cases: {:,}'.format(int(ma_data['Confirmed Cases'][-1])), (dates[-1]+dt.timedelta(hours=12),
     0.85*ma_data['Confirmed Cases'][-1]), color='black', fontsize=9)
 
-plt.semilogy(dates, ma_data['Deaths'], 'r.-')
-plt.semilogy(dates[-1], ma_data['Deaths'][-1], 'r.')
+plt.semilogy(dates, ma_data['Deaths'], 'r.')
+#plt.semilogy(dates[-1], ma_data['Deaths'][-1], 'r.')
 ax.annotate('Deaths: {:,}'.format(int(ma_data['Deaths'][-1])), (dates[-1]+dt.timedelta(hours=12), 0.85*ma_data['Deaths'][-1]),
         color='red', fontsize=9)
 
-plt.semilogy(dates, ma_data['Suffolk'],'g-')
-plt.semilogy(dates[-1], ma_data['Suffolk'][-1], 'g.')
-ax.annotate('Suffolk: {:,}'.format(int(ma_data['Suffolk'][-1])), (dates[-1]+dt.timedelta(hours=12), 0.65*ma_data['Suffolk'][-1]),
-        color='green', fontsize=9)
-
-plt.semilogy(dates, ma_data['Middlesex'],'-', color='orange')
-plt.semilogy(dates[-1], ma_data['Middlesex'][-1], '.', color='orange')
-ax.annotate('Middlesex: {:,}'.format(int(ma_data['Middlesex'][-1])), (dates[-1]+dt.timedelta(hours=12),
-    1.15*ma_data['Middlesex'][-1]), color='orange', fontsize=9)
+#plt.semilogy(dates, ma_data['Suffolk'],'g-')
+#plt.semilogy(dates[-1], ma_data['Suffolk'][-1], 'g.')
+#ax.annotate('Suffolk: {:,}'.format(int(ma_data['Suffolk'][-1])), (dates[-1]+dt.timedelta(hours=12), 0.65*ma_data['Suffolk'][-1]),
+#        color='green', fontsize=9)
+#
+#plt.semilogy(dates, ma_data['Middlesex'],'-', color='orange')
+#plt.semilogy(dates[-1], ma_data['Middlesex'][-1], '.', color='orange')
+#ax.annotate('Middlesex: {:,}'.format(int(ma_data['Middlesex'][-1])), (dates[-1]+dt.timedelta(hours=12),
+#    1.15*ma_data['Middlesex'][-1]), color='orange', fontsize=9)
 
 ax.set_ylim([0.9,7e6])
 ax.xaxis.set_tick_params(rotation=45, labelsize=10)
